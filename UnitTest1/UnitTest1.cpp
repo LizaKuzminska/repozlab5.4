b843d4ac1f30d06f9e138dfe8793c03cff5da04e
LizaKuzminska <yelyzavetakuzminska@gmail.com>
2023-11-20 19:47:04 +0200

    zfba

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5_4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double z;
+			z = P4(1, 2, 2.2);
+			Assert::AreEqual(z, 0.44, 0.00000001);
+		}
+	};
+}
